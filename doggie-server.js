7615cc3c8a32759982267d50435bbcf153a9c099
keschaefer <keschaefer@gmail.com>
2018-11-09 17:09:18 -0700

    add image paths to data set

--- doggie-server.js
+++ doggie-server.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const locations = require('./data.json')
@@ -16,7 +16,7 @@ app.get('/locations', (req, res) =>{
 })
 
 app.use((req, res, next) => {
-   res.status(404).json({ error: { message: 'Not found' }})
+   res.status(404).json({ error: { message: 'data not found' }})
  })
 
 app.use((err, req, res, next) => {
