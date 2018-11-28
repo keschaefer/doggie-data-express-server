b232873e5034f7c90597f70f02db1f2f505978de
keschaefer <keschaefer@gmail.com>
2018-11-28 11:30:43 -0700

    added migration file

--- knexfile.js
+++ knexfile.js
@@ -26,4 +26,5 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
-};
+}
+}
---
+++ migrations/20181128112059_locations.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+   return knex.schema.createTable('locations', (location) => {
+      location.increments('id')
+      location.string('location_name')
+      location.string('location_address')
+      location.string('description')
+      location.string('category')
+      location.integer('category')
+      location.string('image')
+   })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableExists('locations')
+};
