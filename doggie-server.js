0b2b3a610a1f2997933396c884620cacb3364136
keschaefer <keschaefer@gmail.com>
2018-12-09 15:27:27 -0700

    refactored to remove old routes pre database

--- doggie-server.js
+++ doggie-server.js
@@ -1,11 +1,8 @@
-const fs = require('fs')
-const path = require('path')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
 const bodyParser = require('body-parser')
 const cors = require('cors')
-let locations = require('./data.json')
 const queries = require('./queries.js')
 
 app.use(bodyParser.json())
@@ -25,39 +22,6 @@ app.post('/', (req, res) => {
 app.delete('/:id', (req, res) => {
    queries.deleteLocation(req.params.id).then(res.status(204).send())
 })
-
-// app.post('/', (req, res, next) => {
-//    let newLocation = req.body 
-//    locations.push(newLocation)
-//    res.send(newLocation)
-// })
-
-// app.get('/:id', (req, res) =>{
-//    let id = Number(req.params.id)
-//    let locationById = locations.find(location => {
-//       return location.id === id
-//    })
-//    if (locationById) {
-//    res.send({location: locationById})
-//    } else {
-//       res.status(404).send({ error: { message: "Sorry that ID does not exist" }})
-//    }
-// })
-
-// app.put('/:id', (req, res, next) => {
-//    let { id } = req.params
-//    let { description } = req.body
-//    let newObj = locations.map(location => {
-//       if (location.id == id) {
-//          location.description = description
-//       }
-//       return location
-//    })
-//    location = newObj 
-//    res.send({locations})
-// })
-
-
 
 app.use((req, res, next) => {
    res.status(404).json({ error: { message: 'data not found' }})
--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,13 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgresql://localhost/denverdoggie'
   },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
-}
+  }
 
 }
